861933eeab82173ac06c072e421e1ee07fb162c4
malbin.nir <malbin.nir@gmail.com>
2013-12-25 04:25:39 +0000

    stubs

---
+++ tau-compilation-2014a/04-BasicCodeGeneration/src/ic/codegeneration/RunTimeChecksGenerator.java
@@ -0,0 +1,27 @@
+/**
+ * @team Happy Tree Friends
+ * 1. Sagi Katorza
+ * 2. Assaf Krintza
+ * 3. Nir Malbin
+ */
+package ic.codegeneration;
+
+public class RunTimeChecksGenerator {
+
+	public void emitNullCheck(_3ACILGenerator generator, int register) {
+		// TODO implement
+	}
+	
+	public void emitArrayIndexCheck(_3ACILGenerator generator, int register) {
+		// TODO implement
+	}
+	
+	public void emitArrayAllocationCheck(_3ACILGenerator generator, int register) {
+		// TODO implement
+	}
+	
+	public void emitDivisionByZeroCheck(_3ACILGenerator generator, int register) {
+		// TODO implement
+	}
+	
+}
