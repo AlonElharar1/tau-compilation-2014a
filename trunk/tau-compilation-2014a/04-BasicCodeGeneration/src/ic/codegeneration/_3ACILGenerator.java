c0f00a42a387eeb07a57f29a6753c16865147a4f
malbin.nir@gmail.com <malbin.nir@gmail.com@d9e21570-db6a-c890-7fcb-d7c32a4eab2b>
2013-12-25 04:25:39 +0000

    stubs

---
+++ trunk/tau-compilation-2014a/04-BasicCodeGeneration/src/ic/codegeneration/_3ACILGenerator.java
@@ -0,0 +1,179 @@
+/**
+ * @team Happy Tree Friends
+ * 1. Sagi Katorza
+ * 2. Assaf Krintza
+ * 3. Nir Malbin
+ */
+
+package ic.codegeneration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _3ACILGenerator {
+
+	private List<String> data = new ArrayList<String>();
+	private List<Instrucation> instrucations = new ArrayList<Instrucation>();
+	
+	public _3ACILGenerator() {
+	}
+	
+	/**
+	 * Adds somthing to the data section
+	 * @param data
+	 */
+	public void addData(String data) {
+		// TODO implement
+	}
+	
+	/**
+	 * Adds an label
+	 * @param identifer
+	 */
+	public void addLabel(String identifer) {
+		// TODO implement
+	}
+	
+	/**
+	 * Adds an label
+	 * @param identifer
+	 */
+	public void addLabel(int identifer) {
+		// TODO implement
+	}
+	
+	/**
+	 * Adds an instruction
+	 * @param opcode
+	 * @param operands
+	 */
+	public void addOpcode(OpCodes opcode, String... operands) {
+		// TODO implement
+	}
+	
+	/**
+	 * Writes the generated code into file
+	 * @param path
+	 * @throws IOException
+	 */
+	public void write(String path) throws IOException {
+		// TODO implement
+	}
+	
+	private abstract class Instrucation {
+		public abstract String generateString();
+	}
+	
+	private class LabelInstrucation extends Instrucation {
+		private String identifer;
+
+		public LabelInstrucation(String identifer) {
+			this.identifer = identifer;
+		}
+
+		public LabelInstrucation(int identifer) {
+			this(Integer.toString(identifer));
+		}
+
+		public String getIdentifer() {
+			return identifer;
+		}
+
+		@Override
+		public String generateString() {
+			return (String.format(":%s", this.identifer));
+		}
+	}
+	
+	private class OpcodeInstrucation extends Instrucation {
+		
+		private OpCodes opcode;
+		private String[] operands;
+		
+		public OpcodeInstrucation(OpCodes opcode, String[] operands) {
+			this.opcode = opcode;
+			this.operands = operands.clone();
+		}
+
+		public OpCodes getOpcode() {
+			return (this.opcode);
+		}
+
+		public String[] getOperands() {
+			return (this.operands);
+		}
+
+		@Override
+		public String generateString() {
+			String str = this.getOpcode() + " ";
+			
+			for (String operand : this.getOperands()) {
+				str += operand + " ";
+			}
+			
+			return (str.trim());
+		}
+	}
+	
+	public enum OpCodes {
+
+		// Move
+		MOV("=", 2),
+		
+		// Arithmetic
+		ADD("+", 3),
+		SUB("-", 3),
+		MUL("*", 3),
+		DIV("/", 3),
+		MOD("%", 3),
+		NEG("-", 2),
+		EQ("==", 3),
+		NEQ("!=", 3),
+		ST("<", 3),
+		STE("<=", 3),
+		GT(">", 3),
+		GTE(">=", 3),
+
+		// Memory
+		RED("[]", 2),
+		WRT("[]=", 2),
+		
+		// Logic
+		AND("&&", 3),
+		OR("||", 3),
+		NOT("!", 2),
+		
+		// Control
+		GTO("goto", 1),
+		IF("if", 2),
+		NIF("if!", 2),
+		IFE("if=", 3),
+		IFNE("if!=", 3),
+
+		// Invocation
+		PRM("param", 1),
+		CALL("call", 1),
+		CALL2("call", 2),
+		RET("ret", 1),
+		RET2("ret", 2),
+		
+		;
+
+		private String opcode;
+		private int numberOfOperands;
+		
+		private OpCodes(String opcode, int numberOfOperands) {
+			this.opcode = opcode;
+			this.numberOfOperands = numberOfOperands;
+		}
+		
+		public String getOpcode() {
+			return (this.opcode);
+		}
+		
+		public int getNumberOfOperands() {
+			return (this.numberOfOperands);
+		}
+	}
+}
